fac9fcc70bc585bc9f72b27818ade9a7c56947dc
Yeji Hong <yeji.dev@gmail.com>
2024-06-27 00:14:53 +0900

    Title: 입력받은 값과 합 출력, Time: 120ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240627/입력받은 값과 합 출력/output-of-value-and-sum.kt
@@ -0,0 +1,9 @@
+import java.io.*
+import java.util.*
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val st = StringTokenizer(br.readLine())
+    val a = st.nextToken().toInt()
+    val b = st.nextToken().toInt()
+    print("$a $b ${a+b}")
+}
