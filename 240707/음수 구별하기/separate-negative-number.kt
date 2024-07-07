18fff292c8ef019d934194735f5dfa68810408ad
Yeji Hong <yeji.dev@gmail.com>
2024-07-07 19:40:49 +0900

    Title: 음수 구별하기, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

--- 240707/음수 구별하기/separate-negative-number.kt
+++ 240707/음수 구별하기/separate-negative-number.kt
@@ -4,7 +4,8 @@ import java.util.*
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
+    println(n)
     if (n < 0) {
-        print("$n\nminus")
+        print("minus")
     }
 }
