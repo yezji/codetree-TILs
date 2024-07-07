89dacf2b8164c113aa132a99558db2f2dc1060a1
Yeji Hong <yeji.dev@gmail.com>
2024-07-07 19:50:25 +0900

    Title: 정사각형의 넓이, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240707/정사각형의 넓이/area-of-a-rectangle.kt
@@ -0,0 +1,9 @@
+import java.io.*
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    println(n*n)
+    if (n < 5) println("tiny")
+}
