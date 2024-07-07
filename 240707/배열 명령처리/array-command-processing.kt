202d44d6613a964106a9e223f3bb28d7fa91348f
Yeji Hong <yeji.dev@gmail.com>
2024-07-07 16:55:31 +0900

    Title: 배열 명령처리, Time: 298ms, Memory: 20MB, Status: Wrong Answer - CodeTree

--- 240707/배열 명령처리/array-command-processing.kt
+++ 240707/배열 명령처리/array-command-processing.kt
@@ -7,47 +7,38 @@ fun main() {
     var st = StringTokenizer(br.readLine())
     val n = st.nextToken().toInt()
     val m = st.nextToken().toInt()
-    val list = LinkedList<LinkedList<Int>>()
-    (1..n).forEach {
-        val ll = LinkedList<Int>()
-        (1..n).forEach {
-            ll.push(0)
-        }
-        list.push(ll)
-    }
+    val arr = Array<Int>(n) { 0 }
     (0 until m).forEach {
         st = StringTokenizer(br.readLine())
         val a = st.nextToken().toInt()
-        var i = st.nextToken().toInt()
+        val i = st.nextToken().toInt() - 1
         var x = 0
         if (a <= 2) {
-            x = st.nextToken().toInt()
+            x = st.nextToken().toInt() - 1
         }
 
         when (a) {
             1 -> {
-                list[i][x] = 1
+                arr[i] = arr[i] or (1 shl n - x)
             }
 
             2 -> {
-                list[i][x] = 0
+                arr[i] = arr[i] and (1 shr n - x).inv()
             }
 
             3 -> {
-                list[i].addFirst(0)
-                list[i].removeLast()
+                arr[i] = arr[i] shr (1)
             }
 
             else -> {
-                list[i].removeFirst()
-                list[i].addLast(0)
+                arr[i] = arr[i] shl (1)
             }
         }
     }
 
-    val sett = mutableSetOf<LinkedList<Int>>()
+    val sett = mutableSetOf<Int>()
     (0 until n).forEach { i ->
-        sett.add(list[i])
+        sett.add(arr[i])
     }
     print(sett.size)
 }
