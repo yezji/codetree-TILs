0b8ffdf117e358cdcc0fecade96000f0141f9e1f
Yeji Hong <yeji.dev@gmail.com>
2024-07-07 19:29:21 +0900

    Title: 배열 명령처리, Time: 358ms, Memory: 22MB, Status: Wrong Answer - CodeTree

--- 240707/배열 명령처리/array-command-processing.kt
+++ 240707/배열 명령처리/array-command-processing.kt
@@ -21,7 +21,7 @@ fun main() {
             1 -> arr[i] or (1 shl (n-x))
             2 -> arr[i] and ((1 shr (n-x)).inv())
             3 -> arr[i] shr 1
-            else -> (arr[i] shl 1) and ((1 shl n)-1)
+            else -> (arr[i] shl 1) and ((1 shl n-1)-1)
         }
     }
 
