6115f9e21f939af99631904d5b3a5c02d96db964
Yeji Hong <yeji.dev@gmail.com>
2024-07-13 16:01:48 +0900

    Title: 최솟값 3개, Time: 115ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240713/최솟값 3개/top-3-smallest-number.kt
+++ 240713/최솟값 3개/top-3-smallest-number.kt
@@ -3,20 +3,21 @@ import java.util.*
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
-    val stt = TreeSet<Int>()
+    val pq = PriorityQueue<Int>()
     val n = br.readLine().toInt()
     val st = StringTokenizer(br.readLine())
     (1 .. n).forEach {
         val num = st.nextToken().toInt()
-        stt.add(num)
-        (if (stt.size < 3) -1
+        pq.add(num)
+        (if (pq.size < 3) -1
         else {
-            var limit = 3
-            var result = 1L
-            val iter = stt.iterator()
-            while (iter.hasNext() && limit-- > 0) {
-                result *= iter.next()
-            }
+            var a = pq.poll()
+            var b = pq.poll()
+            var c = pq.poll()
+            var result: Long = a * b * c.toLong()
+            pq.add(a)
+            pq.add(b)
+            pq.add(c)
             result
         }).let {
             println(it)
