d52de38a91b685cf59817734e0cacbde45a7ee78
Yeji Hong <yeji.dev@gmail.com>
2024-07-13 16:23:18 +0900

    Title: n x n 표에서 n번째로 큰 수 찾기, Time: 3074ms, Memory: 120MB, Status: Time Limit Exceed - CodeTree

--- 240713/n x n 표에서 n번째로 큰 수 찾기/find-the-n-largest-number-in-then-n-x-n-table.kt
+++ 240713/n x n 표에서 n번째로 큰 수 찾기/find-the-n-largest-number-in-then-n-x-n-table.kt
@@ -4,16 +4,16 @@ import java.util.*
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
-    val pq = PriorityQueue<Int>(Collections.reverseOrder())
+    val stt = TreeSet<Int>()
     val st = StringTokenizer(br.readLine())
     (1 .. n).forEach { i ->
         val item = st.nextToken().toInt()
         (1 .. n).forEach {j -> 
-            pq.add(item/j)
+            stt.add(item/j)
         }
     }
     (1 .. n).forEach{
-        val result = pq.poll()
+        val result = stt.pollLast()
         if (it == n) println(result)
     }
 }
