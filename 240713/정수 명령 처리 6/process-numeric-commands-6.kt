8eb894c37fbbb55bebfa88402c73f59c5eca4801
Yeji Hong <yeji.dev@gmail.com>
2024-07-13 15:39:05 +0900

    Title: 정수 명령 처리 6, Time: 96ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240713/정수 명령 처리 6/process-numeric-commands-6.kt
@@ -0,0 +1,28 @@
+import java.io.*
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val pq = PriorityQueue<Int>()
+    val n = br.readLine().toInt()
+    (1 .. n).forEach {
+        val st = StringTokenizer(br.readLine())
+        when (st.nextToken()) {
+            "push" -> {
+                pq.add(st.nextToken().toInt())
+            }
+            "pop" -> {
+                println(pq.poll())
+            }
+            "size" -> {
+                println(pq.size)
+            }
+            "empty" -> {
+                println(if (pq.isEmpty()) 1 else 0)
+            }
+            "top" -> {
+                println(pq.peek())
+            }
+        }
+    }
+}
