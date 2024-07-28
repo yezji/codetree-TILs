dbc4b745cfb9f7eb20beac3dd4dcc1936cc9df53
Yeji Hong <yeji.dev@gmail.com>
2024-07-28 17:23:44 +0900

    Title: k번째로 작은 쌍의 합, Time: 994ms, Memory: 42MB, Status: Passed - CodeTree

--- 240728/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.kt
+++ 240728/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.kt
@@ -10,7 +10,13 @@ fun main() {
 
     val arr1 = Array(n) {0}
     val arr2 = Array(m) {0}
-    val pq = PriorityQueue<Pair<Int, Int>>() { a, b -> (a.first + a.second) - (b.first + b.second) }
+    val pq = PriorityQueue<Triple<Int, Int, Int>>() { a, b -> if (a.first != b.first) {
+            a.first - b.first
+        } else if (a.second != b.second) {
+            a.second - b.second
+        } else {
+            a.third - b.third
+        } }
     st = StringTokenizer(br.readLine())
     (0 until n).forEach { i ->
         arr1[i] = st.nextToken().toInt()
@@ -22,13 +28,18 @@ fun main() {
     }
     arr2.sort()
 
-    (0 until n).forEach { i ->
-        (0 until m).forEach { j ->
-            pq.add(Pair(arr1[i], arr2[j]))
+    (0 until n).forEach { i->
+        pq.add(Triple(arr1[i] + arr2[0], i, 0))
+    }
+    (0 until k-1).forEach { i ->
+        val mini = pq.poll()
+        var idx1 = mini.second
+        var idx2 = mini.third
+
+        idx2++
+        if (idx2 < m) {
+            pq.add(Triple(arr1[idx1] + arr2[idx2], idx1, idx2))
         }
     }
-    (1 .. k-1).forEach {
-        pq.poll()
-    }
-    println(pq.peek().let {it.first+it.second})
+    println(pq.peek().first)
 }
