e6941db4e0435d63072e1d90f509ef87ded699ad
Yeji Hong <yeji.dev@gmail.com>
2024-08-03 19:24:21 +0900

    Title: 배열 추출 2, Time: 794ms, Memory: 21MB, Status: Passed - CodeTree

--- 240803/배열 추출 2/array-extraction-2.kt
+++ 240803/배열 추출 2/array-extraction-2.kt
@@ -1,9 +1,11 @@
 import java.util.*
 import java.io.*
+import java.math.*
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
-    val pq = PriorityQueue<Int>()
+    val pq = PriorityQueue<Int>() {a, b -> if (Math.abs(a) == Math.abs(b)) {a - b}
+     else {Math.abs(a) - Math.abs(b)}}
     val n = br.readLine().toInt()
     (1 .. n).forEach {
         val num = br.readLine().toInt()
